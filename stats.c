f97c7413e43dbc383d21fc143f88c491caa40c96
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2022-09-18 22:09:15 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,9 +2,41 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+	int i;
+	float min = numberset[0], max = numberset[0], prom, sum = 0;
+
+	//antes de la cuenta comprobar que no sea cero
+	//si es el caso devolver NAN
+	
+	//tomar el arreglo entrante y calcular los maximos minimos y promedio
+
+    for(i = 0 ; i < setlength; i++)
+    {
+        if(numberset[i]> max)
+        {
+            max = numberset[i];
+        }
+        if(numberset[i] < min)
+        {
+            min = numberset[i];
+        }
+      sum += numberset[i];  
+    }
+	if(sum == 0)
+	{
+		s.average = NAN;
+		s.min = NAN;
+		s.max = NAN;
+	}
+	else
+	{
+	prom  = sum/setlength;
+	
+    s.average = prom;
+    s.min = min;
+    s.max = max;
+	}
+	
 }
 
 int emailAlertCallCount = 0;
