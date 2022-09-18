9de58b942d68e2e97996202fc0bc779bf59e612a
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2022-09-18 22:15:34 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,11 +5,6 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 	int i;
 	float min = numberset[0], max = numberset[0], prom, sum = 0;
 
-	//antes de la cuenta comprobar que no sea cero
-	//si es el caso devolver NAN
-	
-	//tomar el arreglo entrante y calcular los maximos minimos y promedio
-
     for(i = 0 ; i < setlength; i++)
     {
         if(numberset[i]> max)
